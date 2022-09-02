35105f2152c45f96c6f594e6cc89e6df7af79f1d
Matthew Cook <cookm353@gmail.com>
2022-09-02 14:22:21 -0400

    added score, updated feedback for guesses

--- static/app.js
+++ static/app.js
@@ -5,7 +5,11 @@ const $tds = $("td");
 const $form = $("form");
 const $formInput = $("#guess");
 const $formBttn = $("#guessForm button");
-const $resultUL = $("#resultList")
+const $resultUL = $("#resultList");
+const $scoreDisplay = $('#score');
+const $invalidWordDisplay = $("#invalidWord");
+
+let score = 0;
 
 const submitURL = 'http://localhost:5000/check-word';
 
@@ -29,24 +33,37 @@ async function checkGuess() {
     const result = (resp.data.result);
     
     showResult(guess, result)
+
+    if (result === "ok") {
+        updateScoreDisplay(guess);
+    }
+
     $formInput.val('');
     
     return resp;
 }
 
+// Show word if it's valid, give error if it isn't
 function showResult(guess, result) {
-    console.log(guess)
     let display_result;
-    switch(result) {
-        case 'ok':
-            display_result = "Okay"            ;
-            break;
-        case 'not-word':
-            display_result = 'Not a Word';
-            break;
-        case 'not-on-board':
-            display_result = 'Not on Board'
-            break;
+
+    if (result === 'ok') {
+        $resultUL.append(`<li>${guess}</li>`)
+    } else {
+        if (result === 'not-word') { display_result = 'Not a Word'; } 
+        else {display_result = 'Not on Board'; }
+        
+        $invalidWordDisplay.fadeIn(0);
+        $invalidWordDisplay.text(display_result);
+        $invalidWordDisplay.fadeOut(2000); 
     }
-    $resultUL.append(`<li>${guess}: ${display_result}</li>`)
 }
+
+function updateScoreDisplay(guess) {
+    let currentScore = $scoreDisplay.text();
+    const wordScore = guess.length;
+    const newScore = parseInt(currentScore) + wordScore;
+
+    $scoreDisplay.text(newScore);
+}
+
