748a84a3fca5a04cc903dee89842423019c05bcc
Matthew Cook <cookm353@gmail.com>
2022-09-08 13:37:15 -0400

    implementing tests

--- test.py
+++ test.py
@@ -8,31 +8,26 @@ app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
 
 class FlaskTests(TestCase):
     # TODO -- write tests for every view function / feature!
-    def test_page_display(self):
+    def test_homepage(self):
         with app.test_client() as client:
             resp = client.get('/')
             html = resp.get_data(as_text=True)
             
             self.assertEqual(resp.status_code, 200)
-            self.assertIn("<h1>Hi!</h1>", html)
-    
-    def test_guess_submit(self):
+            self.assertIn("<h1>Boggle</h1>", html)
+            self.assertIn('board', session)
+            self.assertIsNone(session.get('high_score'))
+            self.assertIsNone(session.get('games_played'))
+            self.assertIn('<h3 id="timer">60</h3>', html)
+        
+    def test_stats(self):
         with app.test_client() as client:
-            # with client.session_transaction() as change_session:
-            #     change_session['guess'] = 'foo'
+            with client.session_transaction() as change_session:
+                change_session['high_score'] = 42
+                change_session['games_played'] = 80
                 
-            resp = client.post("/guess", data={'guess': "foo"})
-            # html = resp.get_data(as_text=True)
-            
-            self.assertEqual(resp.status_code, 302)
-            
-    def test_redirect(self):
-        with app.test_client() as client:
-            resp = client.get('/guess')
-            html = resp.get_data(as_text=True)
-            
-            self.assertEqual(resp.status_code, 302)
-            
-    def test_redirect_follow(self):
-        with app.test_client() as client:
-            resp = client.get('/guess')
+            resp = client.get('/')
+            self.assertEqual(resp.status_code, 200)
+            self.assertIsNotNone(session.get('high_score'))
+            self.assertEqual(session['high_score'], 42)
+            self.assertEqual(session['games_played'], 80)
